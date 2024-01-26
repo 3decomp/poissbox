09fa8236bac494e196f12840515680731fff8244
Paul Bartholomew <p.bartholomew@epcc.ed.ac.uk>
2024-01-26 10:02:33 +0000

    test: Add test for compact scheme application on non-uniform field

--- tests/grad/test_grad_1d.f90
+++ tests/grad/test_grad_1d.f90
@@ -5,9 +5,11 @@ program test_grad_1d
   
   implicit none
 
-  integer, parameter :: n = 128            ! Problem size (nodes)
-  real(pb_dp), parameter :: L = 3.14_pb_dp ! Domain size
-  real(pb_dp), parameter :: dx = L / n     ! Grid spacing
+  real(pb_dp), parameter :: pi = 4 * atan(1.0_pb_dp)
+  
+  integer, parameter :: n = 128        ! Problem size (nodes)
+  real(pb_dp), parameter :: L = 2 * pi ! Domain size
+  real(pb_dp), parameter :: dx = L / n ! Grid spacing
   
   real(pb_dp), dimension(:), allocatable :: f  ! The function
   real(pb_dp), dimension(:), allocatable :: df ! The gradient
@@ -19,6 +21,7 @@ program test_grad_1d
   call init()
 
   call check_constant_field()
+  call check_varying_field()
   
   call fin()
 
@@ -75,5 +78,60 @@ contains
     end if
     
   end subroutine check_constant_field
+
+  subroutine check_varying_field()
+
+    real(pb_dp) :: rms, delta, expect, tol
+
+    real(pb_dp) :: x
+    integer :: i
+
+    x = 0.0_pb_dp
+    do i = 1, n
+       f(i) = sin(x)
+
+       x = x + dx
+    end do
+
+    call grad_1d(f, dx, df)
+
+    x = 0.5_pb_dp * dx
+    rms = 0.0_pb_dp
+    do i = 1, n
+       expect = cos(x)
+       delta = df(i) - expect
+       rms = rms + delta**2
+       
+       x = x + dx
+    end do
+    rms = sqrt(rms / n)
+    tol = 1.0e-11
+    if (rms > tol) then
+       print *, "FAIL: RMS dfdx = ", rms, "variable f", tol
+       passing = .false.
+    else
+       print *, "PASS: RMS dfdx = ", rms, "variable f", tol
+    end if
+
+    call interp_1d(f, df)
+    x = 0.5_pb_dp * dx
+    rms = 0.0_pb_dp
+    do i = 1, n
+       expect = sin(x)
+       delta = df(i) - expect
+       rms = rms + delta**2
+
+       x = x + dx
+    end do
+    rms = sqrt(rms / n)
+    if (rms > tol) then
+       print *, "FAIL: RMS [f] = ", rms, "variable f", tol
+       passing = .false.
+    else
+       print *, "PASS: RMS [f] = ", rms, "variable f", tol
+    end if
+    
+  end subroutine check_varying_field
+  
   
 end program test_grad_1d
